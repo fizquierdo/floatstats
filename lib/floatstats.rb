0936cc3baf4e1b067be3622f40abec851f7e169b
Fernando Izquierdo-Carrasco <fer.izquierdo@gmail.com>
2011-09-19 16:44:42 +0200

    added operations with 2 float vectors

--- lib/floatstats.rb
+++ lib/floatstats.rb
@@ -28,4 +28,30 @@ module Enumerable
   def standard_deviation
     Math.sqrt(self.sample_variance)
   end
+  # operations with 2 enumerable vectors
+  def self.product(x,y)
+    x.zip(y).map{|i,j| i*j}
+  end
+  def self.distance(x,y)
+    x.zip(y).map{|i,j| i-j}
+  end
+  def self.ratio(x,y)
+    x.zip(y).map{|i,j| i.to_f / j.to_f}
+  end
+  def self.pearson_correlation(x, y)
+    xda = x.diffavg
+    yda = y.diffavg
+    num = self.product(xda, yda).sum
+    den = Math.sqrt(xda.sum_of_squares * yda.sum_of_squares)
+    num / den
+  end
+  def self.spearman_rank(x, y)
+    n = x.size
+    num = 6 * self.distance(x.rank, y.rank).sum_of_squares
+    den = n * (n**2 - 1)
+    1.0 - num.to_f/den.to_f 
+  end
+  def self.spearman_rank2(x, y)
+    self.pearson_correlation(x.rank, y.rank)
+  end
 end
